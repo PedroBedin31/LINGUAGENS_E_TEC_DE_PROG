c91a017f4b8e9fd36734b0a9e1f17e230b491042
Pedro Augusto Bedin <pebbedin@gmail.com>
2026-08-06 11:36:17 -0300

    Aula1_PedroBedin

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	float a,b,c;
+    a = 10;
+    b = 6;
+    c = a + b;
+    printf("A soma de %f + %f = %f\n", a,b,c);
+    c = a - b;
+    printf("A subtracao de %f - %f = %f\n", a,b,c);
+    c = a * b;
+    printf("A multiplicacao de %f * %f = %f\n", a,b,c);
+    c = a / b;
+    printf("A divisao de %f / %f = %f\n", a,b,c);
+	return 0;
+}
+
+
+
+
+
+
+
