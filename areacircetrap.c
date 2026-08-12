defa1d8f9e0bedc71d3f7d8ad63159b0c5461230
Pedro Augusto Bedin <pebbedin@gmail.com>
2026-08-12 11:36:43 -0300

    Área do trapézio e do circulo

--- areacircetrap.c
+++ areacircetrap.c
@@ -6,31 +6,29 @@ int main(int argc, char *argv[]) {
 
 // Área do circulo
 
-	float raio,area;
+	float raio,area_circulo,base_maior,base_menor,altura,area_trapezio;
 	
 	printf("\nRaio do circulo e: ");
 	scanf("%f", &raio);
 	
-	area = pi * raio * raio;
-	printf("\nA area do circulo e: %f", area);
+	area_circulo = pi * raio * raio;
+	printf("\nA area do circulo e: %f", area_circulo);
 
-    printf("Agora a area do trapezio")
+    printf("\nAgora a area do trapezio");
 
 // Área do trapézio
 		
-	float base_maior,base_menor,altura,area;
-	
-	printf("Altura do Trapezio: ");
+	printf("\nAltura do Trapezio: ");
 	scanf("%f", &altura);
 
-	printf("Base Maior: ");
+	printf("\nBase Maior: ");
 	scanf("%f", &base_maior);
 	
-	printf("Base Menor: ");
+	printf("\nBase Menor: ");
 	scanf("%f", &base_menor);
 	
-	area = (base_maior + base_menor) * altura / 2;
-	printf("Area do trapezio: %f", area);
+	area_trapezio = (base_maior + base_menor) * altura / 2;
+	printf("\nArea do trapezio: %f", area_trapezio);
 		
 	return 0;
 }
