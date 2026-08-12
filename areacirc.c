a90dfefdebddf23d4f349a26d8d364adc5ae60e0
Pedro Augusto Bedin <pebbedin@gmail.com>
2026-08-12 11:31:23 -0300

    Área do trapézio e do circulo
    Exercício área do trapézio e circulo - 12/08/2026
    Pedro Augusto Bedin - 26001585-2

--- areacirc.c
+++ areacirc.c
@@ -3,7 +3,9 @@
 #define pi 3.141592
 
 int main(int argc, char *argv[]) {
-	
+
+// Área do circulo
+
 	float raio,area;
 	
 	printf("\nRaio do circulo e: ");
@@ -11,6 +13,24 @@ int main(int argc, char *argv[]) {
 	
 	area = pi * raio * raio;
 	printf("\nA area do circulo e: %f", area);
+
+    printf("Agora a area do trapezio")
+
+// Área do trapézio
+		
+	float base_maior,base_menor,altura,area;
 	
+	printf("Altura do Trapezio: ");
+	scanf("%f", &altura);
+
+	printf("Base Maior: ");
+	scanf("%f", &base_maior);
+	
+	printf("Base Menor: ");
+	scanf("%f", &base_menor);
+	
+	area = (base_maior + base_menor) * altura / 2;
+	printf("Area do trapezio: %f", area);
+		
 	return 0;
 }
