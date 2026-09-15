b894a9372aa8e2924949d7bf1d0a67df73f9d1b1
Pedro Augusto Bedin <pebbedin@gmail.com>
2026-09-14 21:17:27 -0300

    Update ListaDeExercicios3.c

--- ListaDeExercicios3.c
+++ ListaDeExercicios3.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Exercicio 5
+
+int calc_notas(int *valor, int *notas100, int *notas50, int *notas20, int *notas10, int *notas5, int *notas2, int *notas1) {
+    
+    *notas100 = *valor / 100; 
+    *valor = *valor % 100;  
+
+    *notas50 = *valor / 50; 
+    *valor = *valor % 50;
+
+    *notas20 = *valor / 20;
+    *valor = *valor % 20;
+
+    *notas10 = *valor / 10;
+    *valor = *valor % 10;
+
+    *notas5 = *valor / 5;
+    *valor = *valor % 5;
+
+    *notas2 = *valor / 2;
+    *valor = *valor % 2;
+
+    *notas1 = *valor / 1;
+    *valor = *valor % 1;
+
+    return 0; 
+}
+
+
+int main() {
+
+    int valor, notas100, notas50, notas20, notas10, notas5, notas2, notas1;
+    printf("Digite o valor: ");
+    scanf("%d", &valor);
+
+    calc_notas(&valor, &notas100, &notas50, &notas20, &notas10, &notas5, &notas2, &notas1);
+
+    printf("Notas de 100: %d\n", notas100);
+    printf("Notas de 50: %d\n", notas50);
+    printf("Notas de 20: %d\n", notas20);
+    printf("Notas de 10: %d\n", notas10);
+    printf("Notas de 5: %d\n", notas5);
+    printf("Notas de 2: %d\n", notas2);
+    printf("Notas de 1: %d\n", notas1);
+
+    return 0;
+}
+
 //exercicios 7 e 8
 
 float calc_inss(float salario){	
